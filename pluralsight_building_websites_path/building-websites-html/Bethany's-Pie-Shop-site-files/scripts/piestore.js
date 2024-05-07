bb39504770c74a8160ab971b6bceb532e285f4f7
dprittj <dprittj@gmail.com>
2024-05-06 20:22:29 -0500

    piestore functionality currently creates array of pieName and quantity

--- pluralsight_building_websites_path/building-websites-html/Bethany's-Pie-Shop-site-files/scripts/piestore.js
+++ pluralsight_building_websites_path/building-websites-html/Bethany's-Pie-Shop-site-files/scripts/piestore.js
@@ -9,73 +9,78 @@ window.addEventListener("DOMContentLoaded", ()=>{
 
         // let total = 0;
         // const pieOrder = [];
+        let quantity = 0;
+        let pieInfo = [];
         
         button.addEventListener("click", (e)=>{
 
-            const quantity = 0;
-            const pieArray = [];
+            // let quantity = 0;
+            // let pieArray = [];
             const btn = e.currentTarget;
 
             const pieName = btn.getAttribute("data-name");
             const piePrice = btn.getAttribute("data-price");
 
-            // setItem("key", value); where value = an []orderArray consisting of []pieArray's, one for each pie ordered.
+            // setItem("key", value); where value = an []orderArray consisting of one []pieInfo for each kind of pie ordered.
             
-            if (pieArray.length == 0){
+            if (pieInfo.length == 0){
                 // if the array is empty, this block adds the pieName and a quantity of 1 for the array; then returns the array and quantity;
 
-                pieArray.push(pieName);
+                pieInfo.push(pieName);
                 quantity += 1;
-                pieArray.push("quantity: " + quantity);
+                pieInfo.push("quantity: " + quantity);
     
-                console.log("Order created.");
+                console.log("Order created: " + pieInfo);
 
-                return pieArray, quantity;
+                return pieInfo, quantity;
 
-            } else if (pieArray.includes(pieName)){
-                // if the array already has the pieName, this code block updates the quantity by one (for each click), and updates the quantity inside the pieArray, and returns the quantity and the array;
+            } else if (pieInfo.includes(pieName)){
+                // if []pieInfo contains pieName, quantity updates by one (for each click), replaces quantity value inside []pieInfo, and returns new, updated values for quantity and []pieInfo;
 
                 quantity +=1;
-                const countIndex = pieArray.indexOf(quantity);
-                pieArray.splice(countIndex, 1, quantity);
+                const countIndex = pieInfo.indexOf(quantity);
+                pieInfo.splice(countIndex, 1, quantity);
 
-                console.log("Quantity updated.");
+                console.log("Quantity updated: "+ pieInfo);
 
-                return pieArray, quantity;
+                return pieInfo, quantity;
 
-            } else if (!(pieArray.includes(pieName)) && pieArray.length > 0){
+            } else if (!(pieInfo.includes(pieName)) && pieArray.length > 0){
                 // this throws an error if the array is not empty, but also does not contain the pieName;
 
                 console.log("error");
 
             };
 
+            cart.setItem("Ordering", JSON.stringify(order));
+
+            console.log("Order saved.")
+            console.log(JSON.stringify(order));
 
 
+            // if(cart.length == 0){
 
-            if(cart.length == 0){
+            //     const order = {
+            //         pie: [pieName],
+            //         price: [piePrice],
+            //         quantity: [quantity]
+            //     };
 
-                const order = {
-                    pie: [pieName],
-                    price: [piePrice],
-                    quantity: [quantity]
-                };
+            //     for (let i = 0; i < order[pie].lenth; i++){
 
-                for (let i = 0; i < order[pie].lenth; i++){
+            //         if (order.pie.includes(pieName)){
+            //             pie.quantity += 1;
 
-                    if (order.pie.includes(pieName)){
-                        pie.quantity += 1;
+            //         }
 
-                    }
+            //     }
 
-                }
+            //     cart.setItem("Ordering", JSON.stringify(order));
 
-                cart.setItem("Ordering", JSON.stringify(order));
+            //     console.log("Order saved.")
+            //     console.log(JSON.stringify(order));
 
-                console.log("Order saved.")
-                console.log(JSON.stringify(order));
-
-            }
+            // }
 
 
 
